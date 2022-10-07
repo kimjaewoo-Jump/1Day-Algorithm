40906cbe6222413133deef54f8e5ecdc49ce20ea
kimjaewoo <kjw9612@naver.com>
2022-10-07 14:51:45 +0900

    commit

---
+++ C:C++/1Day-Algorithm/4-O.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int n, d[200001], ret, cnt;  //d: ) 세기  위함 
+string s;
+stack<int> stk;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); 
+	cin >> n >> s;   
+	for (int i = 0; i < n; i++) {
+		if (s[i] == '(') stk.push(i);  
+		else if(stk.size()){ 
+				d[i] = d[stk.top()] = 1;
+				stk.pop(); 
+		}
+	} 
+	for (int i = 0; i < n; i++){
+		if (d[i]) {
+			cnt++;
+            ret = max(ret, cnt);   
+		}else cnt = 0;  
+	}
+	cout << ret << "\n"; 
+}
+
+
+
+
+
+    
+
